5d3d35d54cdf8aee72412a7016113cffbe6839b6
Aleksey Kravchenko <const346@gmail.com>
2026-04-26 10:39:42 +0200

    Fix tower weapon
    - add position and rotation offset for Thrower.cs

--- Assets/Game/Scripts/Thrower.cs
+++ Assets/Game/Scripts/Thrower.cs
@@ -8,6 +8,9 @@ public class Thrower : MonoBehaviour
     [SerializeField] private AudioClip _throwSound;
     [Space]
     [SerializeField] private bool _faceThrowDirection = true;
+    [SerializeField] private float _returnDelay = 2;
+    [SerializeField] private Vector2 _bulletOffset;
+    [SerializeField] private float _bulletOffsetRotation;
 
     private float _defaultAngle;
     private float _lastThrowTime;
@@ -20,18 +23,20 @@ public class Thrower : MonoBehaviour
 
     public void Throw(Vector2 force)
     {
-        var bullet = Instantiate(_bulletPrefab, _gunAnchor.position, Quaternion.identity);
-        bullet.AddForce(force, ForceMode2D.Impulse);
-
+        var bullet = Instantiate(_bulletPrefab, _gunAnchor.transform.position, Quaternion.identity);
+       
         if (_faceThrowDirection)
         {
             var angle = Vector2.Angle(force, Vector2.right);
-            bullet.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            bullet.transform.rotation = Quaternion.Euler(0f, 0f, angle );
+            bullet.transform.position = _gunAnchor.TransformPoint(_bulletOffset);
 
             var localForce = transform.localScale * force;
             var visualAngle = Vector2.Angle(localForce, Vector2.right);
-            _gunAnchor.transform.localRotation = Quaternion.Euler(0f, 0f, visualAngle);
+            _gunAnchor.transform.localRotation = Quaternion.Euler(0f, 0f, visualAngle - _bulletOffsetRotation);
         }
+
+        bullet.AddForce(force, ForceMode2D.Impulse);
 
         _lastThrowTime = Time.time;
         _isThrowing = false;
@@ -90,12 +95,29 @@ public class Thrower : MonoBehaviour
 
     private void Update()
     {
-        if (!_isThrowing && Time.time - _lastThrowTime > 2f)
+        if (!_isThrowing && Time.time - _lastThrowTime > _returnDelay)
         {
             var currentAngle = _gunAnchor.localRotation.eulerAngles.z;
             var angle = Mathf.LerpAngle(currentAngle, _defaultAngle, Time.deltaTime);
 
             _gunAnchor.localRotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_gunAnchor != null)
+        {
+            var p1 = _gunAnchor.transform.position;
+            var p2 = _gunAnchor.TransformPoint(_bulletOffset);
+
+            Gizmos.DrawLine(p1, p2);
+
+            var rOffset = Quaternion.Euler(0f, 0f, _bulletOffsetRotation);
+            var p3 = _gunAnchor.TransformDirection(rOffset * Vector2.right);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(p2, p3);
         }
     }
 }
