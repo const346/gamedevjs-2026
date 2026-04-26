0e84ae4beab75bf05d079f6a663cf0dcf404e808
Aleksey Kravchenko <const346@gmail.com>
2026-04-26 02:17:46 +0200

    Add main loop sounds and swith light color

--- Assets/Game/Scripts/EnemyWave.cs
+++ Assets/Game/Scripts/EnemyWave.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class EnemyWave : MonoBehaviour, IGameTask
 {
@@ -15,6 +16,16 @@ public class EnemyWave : MonoBehaviour, IGameTask
     [SerializeField] private float _endDelay = 5f;
     [Space]
     [SerializeField] private EnemyContainer[] _enemies;
+    [Space]
+    [SerializeField] private Transform _lightContainer;
+    [SerializeField] private Color _attackColor = Color.red;
+    [Space]
+    [SerializeField] private AudioClip _attackSound;
+    [SerializeField] private AudioClip _endSound;
+    [SerializeField] private AudioClip _attackLoopSound;
+    [SerializeField] private AudioClip _baseLoopSound;
+    [Space]
+    [SerializeField] private AudioSource _musicSource;
 
     [System.Serializable]
     public class EnemyContainer
@@ -23,12 +34,28 @@ public class EnemyWave : MonoBehaviour, IGameTask
         public int Count;
     }
 
+    private Light2D[] _lights;
+    private Color _defaultColor;
+
+    private void Awake()
+    {
+        _lights = _lightContainer.GetComponentsInChildren<Light2D>();
+        _defaultColor = _lights.First().color;
+    }
+
     public IEnumerator Running(EnemyTarget enemyTarget)
     {
         Debug.Log($"ENEMY WAVE | {name} | START");
 
         yield return new WaitForSeconds(_startDelay);
+        _musicSource.Stop();
+        yield return SetLightColor(_defaultColor, _attackColor, 1);
+        AudioSource.PlayClipAtPoint(_attackSound, _spawnPosition);
+        yield return new WaitForSeconds(3);
 
+        _musicSource.clip = _attackLoopSound;
+        _musicSource.Play();
+        
         Debug.Log($"ENEMY WAVE | {name} | SPAWNING");
 
         var enemyPrefabs = _enemies
@@ -59,6 +86,16 @@ public class EnemyWave : MonoBehaviour, IGameTask
         var breakTime = Time.time + _attackDuration;
         yield return new WaitUntil(() => enemies.Count(x => x != null) <= 0 || Time.time >= breakTime || !enemyTarget.IsLive);
 
+        _musicSource.Stop();
+
+        yield return SetLightColor(_attackColor, _defaultColor, 1);
+        AudioSource.PlayClipAtPoint(_endSound, _spawnPosition);
+
+        yield return new WaitForSeconds(3);
+
+        _musicSource.clip = _baseLoopSound;
+        _musicSource.Play();
+
         if (enemies.Any(x => x != null))
         {
             Debug.Log($"ENEMY WAVE | {name} | RETREAT");
@@ -84,5 +121,18 @@ public class EnemyWave : MonoBehaviour, IGameTask
 
         Debug.Log($"ENEMY WAVE | {name} | END");
         yield return new WaitForSeconds(_endDelay);
+    }
+
+    public IEnumerator SetLightColor(Color a, Color b, float duration = 1f)
+    {
+        for (var t = 0f; t < 1f; t += Time.deltaTime / duration)
+        {
+            foreach (var light in _lights)
+            {
+                light.color = Color.Lerp(a, b, t);
+            }
+
+            yield return null;
+        }
     }
 }
